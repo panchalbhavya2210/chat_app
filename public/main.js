8b2f33734db40675716d64468247d20c07ff046b
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-05-12 21:19:46 +0530

    commit net

--- public/main.js
+++ public/main.js
@@ -1,17 +1,42 @@
 const socket = io();
 
 socket.on("message", (message) => {
+  msgBody(message);
   console.log(message);
+
+  let chatContainer = document.querySelector(".chatSection");
+
+  chatContainer.scrollTo({
+    top: 100000000000000000,
+    behavior: "smooth",
+  });
 });
+
+const { username } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+socket.emit("joinRoom", { username });
 
 let formOfMsg = document.getElementById("formOfMsg");
 
 formOfMsg.addEventListener("submit", (e) => {
   e.preventDefault();
   let inputField = document.getElementById("messageField").value;
-  let name = document.getElementById("nameOfUser").value;
-
-  socket.emit("userName", name);
 
   socket.emit("chatMessage", inputField);
 });
+
+function msgBody(message) {
+  let chatContainer = document.querySelector(".chatSection");
+  let divCreator = document.createElement("div");
+
+  divCreator.innerHTML = ` <div class="chatLimiterBg">
+  <span class="msgDetail" id="userName">${message.userName}</span> | <span class="msgDetail" id="timeOfMsg">${message.time}</span><br><br>
+  <p>
+   ${message.text}
+  </p>
+</div>`;
+
+  chatContainer.appendChild(divCreator);
+}
--- server.js
+++ server.js
@@ -2,31 +2,34 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const socket = require("socket.io");
+const formatMsg = require("./utils/format");
 
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+const nameOfBot = "Chat-Bit-Bot";
 
 io.on("connection", (socket) => {
-  socket.emit("message", "Welcome");
+  socket.on("joinRoom", ({ username }) => {
+    socket.emit("message", formatMsg(nameOfBot, `Welcome ${username}`));
 
-  socket.broadcast.emit("userName", (name) => {
-    io.emit("message", name);
-  });
-  // socket.broadcast.emit("new-user", (name) => {
-  //   io.emit("message", "user joined");
-  // });
-  socket.on("disconnect", () => {
-    io.emit("message", "user left");
-  });
-  socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    socket.broadcast.emit("new-user", (name) => {
+      io.emit(
+        "message",
+        formatMsg(nameOfBot, `${username} Has Joined The Chat`)
+      );
+    });
+    socket.on("disconnect", () => {
+      io.emit("message", formatMsg(nameOfBot, `${username} Has Left The Chat`));
+    });
+    socket.on("chatMessage", (msg) => {
+      io.emit("message", formatMsg(`${username}`, msg));
+    });
   });
 });
 
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log("running On " + PORT));
-//
