dcb2bd81f3d8ee5913dd3a73af14b17e621dc289
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-05-14 12:19:55 +0530

    v

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const http = require("http");
 const socket = require("socket.io");
 const formatMsg = require("./utils/format");
-const serverless = require("serverless-http");
-const Router = express.Router();
 
 const app = express();
 const server = http.createServer(app);
@@ -13,31 +11,29 @@ const io = socket(server);
 // app.use(express.static(path.join(__dirname, "public")));
 const nameOfBot = "Chat-Bit-Bot";
 
-Router.get("/", (req, res) => {
-  io.on("connection", (socket) => {
-    socket.on("joinRoom", ({ username }) => {
-      socket.emit("message", formatMsg(nameOfBot, `Welcome ${username}`));
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username }) => {
+    socket.emit("message", formatMsg(nameOfBot, `Welcome ${username}`));
 
-      socket.broadcast.emit("new-user", (name) => {
-        io.emit(
-          "message",
-          formatMsg(nameOfBot, `${username} Has Joined The Chat`)
-        );
-      });
-      socket.on("disconnect", () => {
-        io.emit(
-          "message",
-          formatMsg(nameOfBot, `${username} Has Left The Chat`)
-        );
-      });
-      socket.on("chatMessage", (msg) => {
-        io.emit("message", formatMsg(`${username}`, msg));
-      });
+    socket.broadcast.emit("new-user", (name) => {
+      io.emit(
+        "message",
+        formatMsg(nameOfBot, `${username} Has Joined The Chat`)
+      );
+    });
+    socket.on("disconnect", () => {
+      io.emit("message", formatMsg(nameOfBot, `${username} Has Left The Chat`));
+    });
+    socket.on("chatMessage", (msg) => {
+      io.emit("message", formatMsg(`${username}`, msg));
     });
   });
 });
+
 // const PORT = 3000 || process.env.PORT;
 
 // server.listen(PORT, () => console.log("running On " + PORT));
-app.use("/.netlify/functions/api", Router);
-module.exports.handler = serverless(app);
+app.use(express.static(path.join("__dirname", "public")));
+// module.exports.handler = serverless(app);
+
+module.exports = app;
